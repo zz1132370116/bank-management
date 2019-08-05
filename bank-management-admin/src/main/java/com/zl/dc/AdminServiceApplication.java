243b4ce3f54bf0e11a1a15c0c9980ca689a79eca
zhanglei <zhanglei@iherogroup.com>
2019-08-05 20:30:03 +0800

    配置网关,添加管理员服务

---
+++ bank-management-admin/src/main/java/com/zl/dc/AdminServiceApplication.java
@@ -0,0 +1,23 @@
+package com.zl.dc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @version: V1.0
+ * @author: zhanglei
+ * @className: AdminServiceApplication
+ * @description: 管理员服务启动类
+ * @data: 2019/8/5 20:27
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class AdminServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServiceApplication.class,args);
+    }
+
+}
