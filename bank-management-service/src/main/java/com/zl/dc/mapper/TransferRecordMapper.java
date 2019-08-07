a0e1f5a7cb308844db10dbf6c8b80f03ce14271b
zhanglei <zhanglei@iherogroup.com>
2019-08-07 11:28:53 +0800

    删除无用代码

--- bank-management-service/src/main/java/com/zl/dc/mapper/TransferRecordMapper.java
+++ bank-management-service/src/main/java/com/zl/dc/mapper/TransferRecordMapper.java
@@ -14,18 +14,5 @@ import java.util.List;
  */
 @org.apache.ibatis.annotations.Mapper
 public interface TransferRecordMapper extends Mapper<TransferRecord> {
-    @Select("SELECT tr.* FROM transfer_record tr,bank_user us WHERE #{idCard}=us.id_card AND tr.gmt_create >= #{startDate} AND tr.gmt_create <= #{endDate}")
-    @Results({
-            @Result(column = "transfer_record_id",property = "transferRecordId"),
-            @Result(column = "transfer_record_uuid",property = "transferRecordUuid"),
-            @Result(column = "transfer_record_amount",property = "transferRecordAmount"),
-            @Result(column = "transfer_record_time",property = "transferRecordTime"),
-            @Result(column = "transaction_status",property = "transactionStatus"),
-            @Result(column = "user_id",property = "userId"),
-            @Result(column = "bank_out_card",property = "bankOutCard"),
-            @Result(column = "bank_in_card",property = "bankInCard"),
-            @Result(column = "gmt_create",property = "gmtCreate"),
-            @Result(column = "gmt_modified",property = "gmtModified")
-    })
-    List<TransferRecord> getRecordsByParams( TransferRecord transferRecord);
+
 }
