4344411a487332754eec1f5dbd4b3d75d9b22074
zhanglei <zhanglei@iherogroup.com>
2019-08-13 11:31:28 +0800

    跨境转账---转账

--- bank-management-service/src/main/java/com/zl/dc/controller/CrossBorderTransferRecordController.java
+++ bank-management-service/src/main/java/com/zl/dc/controller/CrossBorderTransferRecordController.java
@@ -41,19 +41,43 @@ public class CrossBorderTransferRecordController {
      * @author: zhanglei
      * @param: [crossBorderTransferRecord]
      * @return:org.springframework.http.ResponseEntity<com.zl.dc.vo.BaseResult>
-     * @description: 换算金额
+     * @description: 根据人民币查询外币
      * @data: 2019/8/11 9:48
      */
-    @PostMapping("/getExchange")
-    public ResponseEntity<BaseResult> getExchange(@RequestBody CrossBorderTransferRecord crossBorderTransferRecord) {
+    @GetMapping("/getExchangeRatePrice/{price}/{type}")
+    public ResponseEntity<BaseResult> getExchangeRatePrice(@PathVariable("price")String price,@PathVariable("type") String type) {
 
-        if (crossBorderTransferRecord != null) {
-            CrossBorderTransferRecord crossBorderTransferRecord1 = crossBorderTransferRecordService.getExchange(crossBorderTransferRecord);
-            if (crossBorderTransferRecord != null) {
+        if (price != "" && type !="") {
+            CrossBorderTransferRecord crossBorderTransferRecord1 = crossBorderTransferRecordService.getExchangeRatePrice(price,type);
+            if (crossBorderTransferRecord1 != null) {
                 return ResponseEntity.ok(new BaseResult(0, "成功").append("data", crossBorderTransferRecord1));
             }
             return ResponseEntity.ok(new BaseResult(1, "请重试"));
         }
         return ResponseEntity.ok(new BaseResult(1, "失败"));
+    }
+    /**
+     * @author: zhanglei
+     * @param: [price, type]
+     * @return:org.springframework.http.ResponseEntity<com.zl.dc.vo.BaseResult>
+     * @description: 根据外币查询人民币
+     * @data: 2019/8/13 9:41
+     */
+    @GetMapping("/getExchangeRateCNY/{price}/{type}")
+    public ResponseEntity<BaseResult> getExchangeRateCNY(@PathVariable("price")String price,@PathVariable("type") String type) {
+        if (price != "" && type !="") {
+            CrossBorderTransferRecord crossBorderTransferRecord1 = crossBorderTransferRecordService.getExchangeRateCNY(price,type);
+            if (crossBorderTransferRecord1 != null) {
+                return ResponseEntity.ok(new BaseResult(0, "成功").append("data", crossBorderTransferRecord1));
+            }
+            return ResponseEntity.ok(new BaseResult(1, "请重试"));
+        }
+        return ResponseEntity.ok(new BaseResult(1, "失败"));
+    }
+    @PostMapping("/CrossBorderTransfer")
+    public ResponseEntity<BaseResult> CrossBorderTransfer(@RequestBody CrossBorderTransferRecord crossBorderTransferRecord){
+        if (crossBorderTransferRecord != null){
+            crossBorderTransferRecordService.CrossBorderTransfer(crossBorderTransferRecord);
+        }
     }
 }
--- bank-management-service/src/main/java/com/zl/dc/service/CrossBorderTransferRecordService.java
+++ bank-management-service/src/main/java/com/zl/dc/service/CrossBorderTransferRecordService.java
@@ -3,11 +3,17 @@ package com.zl.dc.service;
 import com.zl.dc.api.exchangeRateApi;
 import com.zl.dc.mapper.BankCardMapper;
 import com.zl.dc.mapper.CrossBorderTransferRecordMapper;
+import com.zl.dc.pojo.BankCard;
 import com.zl.dc.pojo.CrossBorderTransferRecord;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
+
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.util.Date;
+import java.util.UUID;
 
 
 /**
@@ -22,6 +28,8 @@ import java.math.BigDecimal;
 public class CrossBorderTransferRecordService {
     @Resource
     private CrossBorderTransferRecordMapper crossBorderTransferRecordMapper;
+    @Resource
+    private BankCardMapper bankCardMapper;
 
     /**
      * @author: zhanglei
@@ -42,38 +50,72 @@ public class CrossBorderTransferRecordService {
         }
         return crossBorderTransferRecord;
     }
+
+    /**
+     * @author: zhanglei
+     * @param: [price, type]
+     * @return:com.zl.dc.pojo.CrossBorderTransferRecord
+     * @description: 根据人民币查询外币
+     * @data: 2019/8/13 9:41
+     */
+    public CrossBorderTransferRecord getExchangeRatePrice(String price, String type) {
+        //类型转换
+        //根据人民币转为用户选择币种的金额
+        String s = exchangeRateApi.exChangeRatePrice(price,"CNY" , type );
+        CrossBorderTransferRecord crossBorderTransferRecord = new CrossBorderTransferRecord();
+        crossBorderTransferRecord.setCurrencyType(type);
+        crossBorderTransferRecord.setTransferRecordAmountFrom(new BigDecimal(price));
+        crossBorderTransferRecord.setTransferRecordAmountTo(new BigDecimal(s));
+        return crossBorderTransferRecord;
+    }
+    /**
+     * @author: zhanglei
+     * @param: [price, type]
+     * @return:com.zl.dc.pojo.CrossBorderTransferRecord
+     * @description: 根据外币查询人民币
+     * @data: 2019/8/13 9:42
+     */
+    public CrossBorderTransferRecord getExchangeRateCNY(String price, String type) {
+        //类型转换
+        //根据人民币转为用户选择币种的金额
+        String s = exchangeRateApi.exChangeRatePrice(price,type , "CNY" );
+        CrossBorderTransferRecord crossBorderTransferRecord = new CrossBorderTransferRecord();
+        crossBorderTransferRecord.setCurrencyType(type);
+        crossBorderTransferRecord.setTransferRecordAmountFrom(new BigDecimal(s));
+        crossBorderTransferRecord.setTransferRecordAmountTo(new BigDecimal(price));
+        return crossBorderTransferRecord;
+    }
     /**
      * @author: zhanglei
      * @param: [crossBorderTransferRecord]
-     * @return:com.zl.dc.pojo.CrossBorderTransferRecord
-     * @description: 换算金额
-     * @data: 2019/8/12 9:34
+     * @return:void
+     * @description: 跨境转账
+     * @data: 2019/8/13 10:15
      */
-    public CrossBorderTransferRecord getExchange(CrossBorderTransferRecord crossBorderTransferRecord) {
-
-        String from = "CNY";
-        //和0，Zero比较
-        int f = crossBorderTransferRecord.getTransferRecordAmountFrom().compareTo(BigDecimal.ZERO);
-        //和0，Zero比较
-        int t = crossBorderTransferRecord.getTransferRecordAmountTo().compareTo(BigDecimal.ZERO);
-        //判断交易额是否存在
-        if (f == 0 || f == -1){
-            //判断到账金额是否为空
-            if (t == 0 || t == -1){
-                //根据外币向人民币转换
-                String s = exchangeRateApi.exChangeRatePrice(String.valueOf(t), crossBorderTransferRecord.getCurrencyType(), from );
-               crossBorderTransferRecord.setTransferRecordAmountFrom(new BigDecimal(s));
-                return crossBorderTransferRecord;
-            }else{
-                return null;
+    public void CrossBorderTransfer(CrossBorderTransferRecord crossBorderTransferRecord) {
+        Example example = new Example(BankCard.class);
+        Example.Criteria criteria = example.createCriteria();
+        //非空判断
+        if (StringUtils.isNotBlank(crossBorderTransferRecord.getBankOutCard()) &&StringUtils.isNotBlank(crossBorderTransferRecord.getBankInCard()) && StringUtils.isNotBlank(crossBorderTransferRecord.getCurrencyType())){
+            int from = crossBorderTransferRecord.getTransferRecordAmountFrom().compareTo(BigDecimal.ZERO);
+            int to = crossBorderTransferRecord.getTransferRecordAmountTo().compareTo(BigDecimal.ZERO);
+            //非0判断
+            if (from !=0 && from != -1 && to !=0 && to!= -1){
+                //转账
+                //修改银行卡金额
+                criteria.andEqualTo("bankCardNumber",crossBorderTransferRecord.getBankOutCard());
+                BankCard bankCard = bankCardMapper.selectOneByExample(example);
+                bankCard.setBankCardBalance(bankCard.getBankCardBalance().subtract(crossBorderTransferRecord.getTransferRecordAmountFrom()));
+                bankCardMapper.updateByPrimaryKeySelective(bankCard);
+                //创建转账机记录
+                //生成流水号
+                crossBorderTransferRecord.setTransferRecordUuid(UUID.randomUUID().toString());
+                //生成创建时间
+                crossBorderTransferRecord.setGmtCreate(new Date());
+                //
+                crossBorderTransferRecordMapper.insertSelective(crossBorderTransferRecord);
             }
-        }else{
-            //存在
-            //根据外币向人民币转换
-            String s = exchangeRateApi.exChangeRatePrice(String.valueOf(t),from , crossBorderTransferRecord.getCurrencyType() );
-            crossBorderTransferRecord.setTransferRecordAmountTo(new BigDecimal(s));
-            return crossBorderTransferRecord;
+
         }
     }
-
 }
