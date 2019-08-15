2ee8d435d727dd5a13f0397bdbe3a6ec9cc2c6e9
pds <577950300@qq.com>
2019-08-15 15:04:33 +0800

    对登录的和实名认证的一些修改

--- bank-management-auth/bank-management-server/src/main/java/com/zl/dc/controller/AuthController.java
+++ bank-management-auth/bank-management-server/src/main/java/com/zl/dc/controller/AuthController.java
@@ -59,37 +59,29 @@ public class AuthController {
     public ResponseEntity<BaseResult> login(@RequestBody BankUser bankUser) {
         if (bankUser != null) {
             //如果是使用手机加密码登录
-            if (bankUser.getUserPassword() != null && !"".equals(bankUser.getUserPhone())){
+            if (StringUtils.isNotBlank(bankUser.getUserPhone())){
                 //验证手机号是否正确
                 String regex = "^[1][3,4,5,7,8][0-9]{9}$";
                 if (!bankUser.getUserPhone().matches(regex)){
-                    return ResponseEntity.ok(new BaseResult(1, "该手机号不正确"));
+                    return ResponseEntity.ok(new BaseResult(4, "该手机号不正确"));
                 }
-            }else if(bankUser.getIdCard() != null && !"".equals(bankUser.getIdCard())){
+            }else if(StringUtils.isNotBlank(bankUser.getIdCard())){
                 //验证身份证是否正确
                 String regex = "(^\\d{18}$)|(^\\d{15}$)";
                 if (!bankUser.getIdCard().matches(regex)){
-                    return ResponseEntity.ok(new BaseResult(1, "该身份证号不正确"));
+                    return ResponseEntity.ok(new BaseResult(4, "该身份证号不正确"));
                 }
             }
-            /*//1登录操作--获得token和用户信息
-            Map<String, Object> map = this.authService.login(bankUser);
-            String token = (String) map.get("token");
-            BankUser user = (BankUser) map.get("user");
-            //2 有token，返回
-            if (StringUtils.isNotBlank(token)) {
-                BaseResult baseResult = new BaseResult(0, "登录成功").append("token", token).append("user",user);
-                return ResponseEntity.ok(baseResult);
-            }*/
+
             if (bankUser.getUserPassword() == null || "".equals(bankUser.getUserPassword())){
-                return ResponseEntity.ok(new BaseResult(1, "密码不能为空"));
+                return ResponseEntity.ok(new BaseResult(5, "密码不能为空"));
             }
             //登录
             BaseResult baseResult = this.authService.login(bankUser);
             return ResponseEntity.ok(baseResult);
         }
         //3 没有token，失败
-        return ResponseEntity.ok(new BaseResult(1, "登录失败"));
+        return ResponseEntity.ok(new BaseResult(2, "登录失败，请重试"));
     }
 
     /**
@@ -102,26 +94,23 @@ public class AuthController {
     @PostMapping("/loginBySendSms")
     public ResponseEntity<BaseResult> loginBySendSms(@RequestBody BankUserVo bankUser) {
         if (bankUser != null) {
-            //判断验证码是否为空
-            if (bankUser.getCode() == null || "".equals(bankUser.getCode())) {
-                return ResponseEntity.ok(new BaseResult(1, "验证码不能为空"));
+            if (StringUtils.isNotBlank(bankUser.getUserPhone())){
+                //验证手机号是否正确
+                String regex = "^[1][3,4,5,7,8][0-9]{9}$";
+                if (!bankUser.getUserPhone().matches(regex)){
+                    return ResponseEntity.ok(new BaseResult(4, "该手机号不正确"));
+                }
             }
-            /*//1登录操作--获得token和用户信息
-            Map<String, Object> map = this.authService.loginBySendSms(bankUser);
-            BankUser user = (BankUser) map.get("user");
-            String token = (String) map.get("token");
-            //2 有token，返回
-            if (StringUtils.isNotBlank(token)) {
-                BaseResult baseResult = new BaseResult(0, "登录成功")
-                        .append("token", token).append("user",user);
-                return ResponseEntity.ok(baseResult);
-            }*/
+            //判断验证码是否为空
+            if (StringUtils.isNotBlank(bankUser.getCode())) {
+                return ResponseEntity.ok(new BaseResult(3, "验证码不能为空"));
+            }
 
             //登录
             BaseResult baseResult = this.authService.loginBySendSms(bankUser);
             return ResponseEntity.ok(baseResult);
         }
         //3 没有token，失败
-        return ResponseEntity.ok(new BaseResult(1, "登录失败"));
+        return ResponseEntity.ok(new BaseResult(2, "登录失败"));
     }
 }
